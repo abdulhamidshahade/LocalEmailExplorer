2373008dbd989e21b8d61b13c4f2e6776cb58a7f
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-16 02:39:41 +0300

    Add ApplicationDbContext

---
+++ src/LocalEmailExplorer.Services/LocalEmailExplorer.Services.EmailAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LocalEmailExplorer.Services.EmailAPI.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocalEmailExplorer.Services.EmailAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Email> Emails { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
