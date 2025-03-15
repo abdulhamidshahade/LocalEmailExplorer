38f948579d0d4c88b35478c60f9754ac0042a842
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-16 02:34:27 +0300

    Add Email domain model

---
+++ src/LocalEmailExplorer.Services/LocalEmailExplorer.Services.EmailAPI/Models/Email.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocalEmailExplorer.Services.EmailAPI.Models.DTOs
+{
+    public class Email : EmailBase
+    {
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+
+        public Email()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
