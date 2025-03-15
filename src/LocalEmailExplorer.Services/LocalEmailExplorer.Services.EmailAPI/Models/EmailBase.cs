fea5513dcc1a69036a1c35e53e5ce979438c7db4
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-16 02:34:20 +0300

    Add EmailBase abstract

---
+++ src/LocalEmailExplorer.Services/LocalEmailExplorer.Services.EmailAPI/Models/EmailBase.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalEmailExplorer.Services.EmailAPI.Models.DTOs
+{
+    public abstract class EmailBase
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string RecoveryEmail { get; set; }
+    }
+}
