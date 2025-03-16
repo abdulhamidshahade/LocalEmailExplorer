4395b3cb5a013e076c1b1924a71f282bbac22945
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-16 03:19:13 +0300

    Configure EmailAPI to run on Swagger inteface in debug mode

--- src/LocalEmailExplorer.Services/LocalEmailExplorer.Services.EmailAPI/Program.cs
+++ src/LocalEmailExplorer.Services/LocalEmailExplorer.Services.EmailAPI/Program.cs
@@ -9,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -27,7 +29,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalEmailExplorer v1 EmailAPI");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
