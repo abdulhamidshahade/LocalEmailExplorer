94e799ba75cb2c59f554de1afb47ea9bf913e670
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-03-16 03:14:32 +0300

    Add IEmailService interface

---
+++ src/LocalEmailExplorer.Services/LocalEmailExplorer.Services.EmailAPI/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,17 @@
+﻿using LocalEmailExplorer.Services.EmailAPI.Models.DTOs;
+
+namespace LocalEmailExplorer.Services.EmailAPI.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        Task<Email> CreateEmailAsync(EmailDto emailDto);
+        Task<bool> UpdateEmailAsync(EmailDto emailDto, string id);
+        Task<bool> DeleteEmailAsync(EmailDto emailDto);
+        Task<Email> GetEmailByIdAsync(string id);
+        Task<List<Email>> GetEmailsByPhoneAsync(string phoneNumber);
+        Task<List<Email>> GetEmailsByRecoveryEmailAsync(string recoveryEmail);
+        Task<List<Email>> GetEmailsAsync();
+        Task<bool> IsEmailExistsByEmailAddressAsync(string emailAddress);
+        Task<bool> IsEmailExistsByIdAsync(string id);
+    }
+}
