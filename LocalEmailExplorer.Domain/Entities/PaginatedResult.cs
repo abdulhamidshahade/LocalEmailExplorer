879398daadd467a9473be2eb558765537993d8cf
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-14 01:48:59 +0300

    Add PaginatedResult<T> class for pagination support

---
+++ LocalEmailExplorer.Domain/Entities/PaginatedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace LocalEmailExplorer.Domain.Entities
+{
+    public class PaginatedResult<T>
+    {
+        public List<T> Items { get; set; }
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
