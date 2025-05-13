ad0ce012e0090a6b939f8c58f1b15f3938561d68
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-14 01:55:21 +0300

    Add ResponseDto class for API response handling

---
+++ LocalEmailExplorer.Application/DTOs/ResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LocalEmailExplorer.API.DTOs
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public object Data { get; set; }
+        public List<string> Errors { get; set; } = new();
+    }
+}
