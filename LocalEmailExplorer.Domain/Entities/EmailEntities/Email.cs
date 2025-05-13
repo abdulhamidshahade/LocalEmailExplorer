4b5c75c3172eced4ef1a03511c6e980dc9758825
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-14 01:48:06 +0300

    Add Email and EmailBase classes for email management

---
+++ LocalEmailExplorer.Domain/Entities/EmailEntities/Email.cs
@@ -0,0 +1,11 @@
+﻿using LocalEmailExplorer.Domain.Entities.Base;
+
+namespace LocalEmailExplorer.Domain.Entities.EmailEntities
+{
+    public class Email : EmailBase, IAuditableEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ LocalEmailExplorer.Domain/Entities/EmailEntities/EmailBase.cs
@@ -0,0 +1,12 @@
+﻿namespace LocalEmailExplorer.Domain.Entities.EmailEntities
+{ 
+    public abstract class EmailBase
+    {
+        public string EmailAddress { get; set; }
+        public string? Password { get; set; }
+        public string? Phone { get; set; }
+        public string? RecoveryEmail { get; set; } 
+        public bool IsActive { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
