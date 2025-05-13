4bed551d9f897eb47459fc20a913fb1c24f04eb4
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-14 01:52:44 +0300

    Add UnitOfWork class for database operations

---
+++ LocalEmailExplorer.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using LocalEmailExplorer.Infrastructure.Data;
+
+namespace LocalEmailExplorer.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
