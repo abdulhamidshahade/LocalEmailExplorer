1ac634238f382c1c903766d9346bc41c1cfcf922
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-14 01:48:35 +0300

    Add AuthEntities classes for roles, users, and JWT options

---
+++ LocalEmailExplorer.Domain/Entities/AuthEntities/ApplicationRole.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Microsoft.AspNetCore.Identity;
+
+namespace LocalEmailExplorer.Domain.Entities.AuthEntities
+{
+    public class ApplicationRole : IdentityRole<int>
+    {
+        public ApplicationRole()
+        {
+
+        }
+
+        public ApplicationRole(string role) : base(role)
+        {
+
+        }
+    }
+}
