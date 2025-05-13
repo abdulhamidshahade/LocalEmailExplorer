7d679eb8110b5a958e204dca6e3d263924e70b3b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-14 01:51:59 +0300

    Add ApplicationDbContext

---
+++ LocalEmailExplorer.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using LocalEmailExplorer.Domain.Entities.AuthEntities;
+using LocalEmailExplorer.Domain.Entities.Base;
+using LocalEmailExplorer.Domain.Entities.EmailEntities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace LocalEmailExplorer.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Email> Emails { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries<IAuditableEntity>().
+                Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            foreach (var entry in entries)
+            {
+                var entity = entry.Entity;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entity.CreatedAt = DateTime.UtcNow;
+                }
+
+                entity.UpdatedAt = DateTime.UtcNow;
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
