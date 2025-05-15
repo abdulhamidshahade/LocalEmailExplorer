db621efe598d6a4adf1eac8c1e632eff7389a447
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-15 22:54:55 +0300

    Add DI registration for auth and email services

---
+++ LocalEmailExplorer.Application/DI/ApplicationLayerRegistration.cs
@@ -0,0 +1,26 @@
+﻿using LocalEmailExplorer.Application.Services.Concretes.AuthServiceConcretes;
+using LocalEmailExplorer.Application.Services.Concretes.EmailServiceConretes;
+using LocalEmailExplorer.Application.Services.Interfaces.AuthServiceInterfaces;
+using LocalEmailExplorer.Application.Services.Interfaces.EmailServiceInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace LocalEmailExplorer.Application.DI
+{
+    public static class ApplicationLayerRegistration
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection serviceDescriptors)
+        {
+            serviceDescriptors.AddScoped<IAuthService, AuthService>();
+            serviceDescriptors.AddScoped<IRoleService, RoleService>();
+            serviceDescriptors.AddScoped<ITokenService, TokenService>();
+            serviceDescriptors.AddScoped<IUserService, UserService>();
+
+            serviceDescriptors.AddScoped<IEmailService, EmailService>();
+
+            serviceDescriptors.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            return serviceDescriptors;
+        }
+    }
+}
