5bb62aa0208f32367331832ca1287ac27815d2b5
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-15 22:55:34 +0300

    Add EmailNormalizer class for email normalization

---
+++ LocalEmailExplorer.Application/Halpers/EmailNormalizer.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace LocalEmailExplorer.Application.Halpers
+{
+    public class EmailNormalizer
+    {
+        public static string NormalizeEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return string.Empty;
+
+            email = email.Trim().ToLower();
+            email = email.Normalize(NormalizationForm.FormKC);
+
+            var parts = email.Split('@');
+
+            if (parts.Length != 2)
+                return email;
+
+            string localPart = parts[0];
+            string domain = parts[1];
+
+            if (domain == "gmail.com")
+            {
+                localPart = localPart.Replace(".", "");
+            }
+
+            return $"{localPart}@{domain}";
+        }
+    }
+}
