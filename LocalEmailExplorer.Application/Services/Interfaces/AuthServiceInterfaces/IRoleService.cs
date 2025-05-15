9fde4470e7e8c6f867c5a9218e290b4399121442
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-15 22:52:13 +0300

    Add IRoleService interface for role management

---
+++ LocalEmailExplorer.Application/Services/Interfaces/AuthServiceInterfaces/IRoleService.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+using LocalEmailExplorer.Domain.Entities.AuthEntities;
+
+namespace LocalEmailExplorer.Application.Services.Interfaces.AuthServiceInterfaces
+{
+    public interface IRoleService
+    {
+        Task EnsureRoleExistsAsync(string roleName);
+        Task<bool> AssignRoleToUserAsync(ApplicationUser user, string roleName);
+        bool IsValidRole(string roleName);
+    }
+}
