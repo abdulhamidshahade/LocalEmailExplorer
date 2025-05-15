3ffd98945e1a8dad5a4940fad19ad6b068735422
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-15 22:52:28 +0300

    Add ITokenService interface for token generation

---
+++ LocalEmailExplorer.Application/Services/Interfaces/AuthServiceInterfaces/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+using LocalEmailExplorer.Domain.Entities.AuthEntities;
+
+namespace LocalEmailExplorer.Application.Services.Interfaces.AuthServiceInterfaces
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateToken(ApplicationUser user);
+    }
+}
