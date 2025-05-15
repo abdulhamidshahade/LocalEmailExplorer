39d01b72dc0b3795ff5e9deb5ab5bcce2454817b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-15 22:52:03 +0300

    Add IAuthService interface for authentication methods

---
+++ LocalEmailExplorer.Application/Services/Interfaces/AuthServiceInterfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿
+
+using LocalEmailExplorer.Application.Dtos.AuthDtos;
+
+namespace LocalEmailExplorer.Application.Services.Interfaces.AuthServiceInterfaces
+{
+    public interface IAuthService
+    {
+        Task<RegisterResponseDto> Register(RegisterRequestDto requestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto requestDto);
+    }
+}
