e1d651c728cbcbe6ce43ce0da91cfc9c171f3d41
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-15 22:50:50 +0300

    Add InfrastructureLayerRegistration for DI setup

---
+++ LocalEmailExplorer.Infrastructure/DI/InfrastructureLayerRegistration.cs
@@ -0,0 +1,39 @@
+﻿
+
+using LocalEmailExplorer.Domain.Repositories;
+using LocalEmailExplorer.Domain.Repositories.UserEmailInterfaces;
+using LocalEmailExplorer.Infrastructure.Data;
+using LocalEmailExplorer.Infrastructure.Repositories;
+using LocalEmailExplorer.Infrastructure.Repositories.UserEmailConcretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LocalEmailExplorer.Infrastructure.DI
+{ 
+    public static class InfrastructureLayerRegistration
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection serviceDescriptors)
+        {
+
+            serviceDescriptors.AddDbContext<ApplicationDbContext>((provider, options) =>
+            {
+                var configuration = provider.GetRequiredService<IConfiguration>();
+                var connectionString = configuration.GetConnectionString("MSSQLServer");
+
+                options.UseSqlServer(connectionString,
+                    sqloptions => sqloptions.MigrationsAssembly("BlueBerry24.Infrastructure"));
+            });
+
+            serviceDescriptors.AddScoped<IEmailRepository, EmailRepository>();
+           
+            serviceDescriptors.AddScoped<IUnitOfWork, UnitOfWork>();
+
+           
+
+
+
+            return serviceDescriptors;
+        }
+    }
+}
