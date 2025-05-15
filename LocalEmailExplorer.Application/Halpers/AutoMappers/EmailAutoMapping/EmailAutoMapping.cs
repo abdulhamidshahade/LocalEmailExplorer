582c20902588346a57874986d97d7573eb981822
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-15 22:55:10 +0300

    Add EmailAutoMapping class for DTO mappings

---
+++ LocalEmailExplorer.Application/Halpers/AutoMappers/EmailAutoMapping/EmailAutoMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using LocalEmailExplorer.Application.DTOs.EmailDtos;
+using LocalEmailExplorer.Domain.Entities.EmailEntities;
+
+namespace LocalEmailExplorer.Application.Halpers.AutoMappers.EmailAutoMapping
+{
+    public class EmailAutoMapping : Profile
+    {
+        public EmailAutoMapping()
+        {
+            CreateMap<Email, EmailDto>().ReverseMap();
+            CreateMap<Email, CreateEmailDto>().ReverseMap();
+            CreateMap<Email, UpdateEmailDto>().ReverseMap();
+            CreateMap<Email, DeleteEmailDto>().ReverseMap();
+        }
+    }
+}
