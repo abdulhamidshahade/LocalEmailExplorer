1eb973450f718a41f6e5d448fd385df7524bfc38
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-05-15 22:53:45 +0300

    Add RoleService for user role management

---
+++ LocalEmailExplorer.Application/Services/Concretes/AuthServiceConcretes/RoleService.cs
@@ -0,0 +1,49 @@
+﻿
+using LocalEmailExplorer.Application.Services.Interfaces.AuthServiceInterfaces;
+using LocalEmailExplorer.Domain.Entities.AuthEntities;
+using Microsoft.AspNetCore.Identity;
+
+namespace LocalEmailExplorer.Application.Services.Concretes.AuthServiceConcretes
+{
+    public class RoleService : IRoleService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public RoleService(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task<bool> AssignRoleToUserAsync(ApplicationUser user, string roleName)
+        {
+            if (user == null || string.IsNullOrEmpty(roleName))
+            {
+                throw new ArgumentException("User and role name must be provided");
+            }
+
+            await EnsureRoleExistsAsync(roleName);
+
+            if (!await _userManager.IsInRoleAsync(user, roleName))
+            {
+                var result = await _userManager.AddToRoleAsync(user, roleName);
+                return result.Succeeded;
+            }
+
+            return true;
+        }
+
+        public async Task EnsureRoleExistsAsync(string roleName)
+        {
+            if (!await _roleManager.RoleExistsAsync(roleName))
+            {
+                await _roleManager.CreateAsync(new ApplicationRole(roleName));
+            }
+        }
+
+        public bool IsValidRole(string roleName)
+        {
+            return !string.IsNullOrEmpty(roleName) && roleName.Length <= 50;
+        }
+    }
+}
